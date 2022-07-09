cbda3668e3cc51e11a299c2204e48a9c895fcb11
Uspectacle <mario.larsen@ensea.fr>
2022-07-09 23:42:01 +0200

    SCSS navigation
    the menu is now better looking

--- frontend/_fullscreen.js
+++ frontend/_fullscreen.js
@@ -1,25 +1,27 @@
-export {
-    toggleFullScreen,
-}
-
+export { toggleFullScreen };
 
 function toggleFullScreen(document) {
-    if (document.fullscreenElement) {
-        if (document.exitFullscreen) {
-          document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) { /* Safari */
-          document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) { /* IE11 */
-          document.msExitFullscreen();
-        }
+  if (document.fullscreenElement) {
+    localStorage.removeItem("fullScreen");
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      /* IE11 */
+      document.msExitFullscreen();
     }
-    else {
-        if (document.documentElement.requestFullscreen) {
-            document.documentElement.requestFullscreen();
-        } else if (document.documentElement.webkitRequestFullscreen) { /* Safari */
-            document.documentElement.webkitRequestFullscreen();
-        } else if (document.documentElement.msRequestFullscreen) { /* IE11 */
-            document.documentElement.msRequestFullscreen();
-        }
+  } else {
+    localStorage.setItem("fullScreen", true);
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      /* Safari */
+      document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      /* IE11 */
+      document.documentElement.msRequestFullscreen();
     }
+  }
 }
